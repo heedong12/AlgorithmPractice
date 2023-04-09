226a2f2c061b399765e1152a9cac08f20a4f9488
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2023-04-10 00:34:30 +0900

    [Bronze I] Title: 단어순서 뒤집기, Time: 128 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -8,15 +8,16 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         //전체 케이스 개수
         int N = Integer.parseInt(br.readLine());
-
+        //N번 반복
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            //단어 저장할 리스트 생성
             ArrayList<String> list = new ArrayList<>();
             while(st.hasMoreTokens()){
-                list.add(st.nextToken());
+                list.add(st.nextToken());   //단어 저장
             }
-
             bw.write("Case #"+String.valueOf(i+1)+": ");
+            //저장된 단어 순서 뒤집어서 출력
             for (int j=list.size()-1;j>=0;j--){
                 bw.write(list.get(j)+" ");
             }
