c95b9ee2954fbd9b15fd026fe8a916bf7231662e
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2023-05-31 16:16:14 +0900

    [Gold V] Title: 거의 소수, Time: 444 ms, Memory: 53484 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -15,22 +15,22 @@ public class Main {
         //소수 구하기
         for (int i = 2; i <= b; i++) {
             int j=2;
-            while(true){
-                if (i*j>b) break;
-                array[i*j]=1;
+            while(array[i]==0){
+                if (i*j>b) break;   //B의 제곱근보다 크면 while문 종료
+                array[i*j]=1;   //i의 배수들을 1로 설정
                 j++;
             }
         }
 
-        int count=0;
+        int count=0;    //거의 소수
         for (int i = 2; i <= array.length-1; i++) {
             long result=0;
             int j=2;
-            if(array[i]==0){//소수이면
+            if(array[i]==0){    //소수이면
                 while(true){
-                    result =(long) Math.pow(i,j);
-                    if(result>B) break;
-                    if(result>=A) {
+                    result =(long) Math.pow(i,j);   //소수의 2제곱, 3제곱 ..
+                    if(result>B) break;     //소수의 N제곱이 B보다 크면 종료
+                    if(result>=A) {     //소수의 N제곱이 A보다 크면 count +1
                         count++;
                     }
                     j++;
