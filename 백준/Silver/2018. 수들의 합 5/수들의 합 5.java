1a27f68fe166bbf820969fd58dd4aafaf5137f73
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2023-08-03 22:53:40 +0900

    [Silver V] Title: 수들의 합 5, Time: 212 ms, Memory: 15484 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -9,11 +9,14 @@ public class Main {
         int count = 1;  //자기 자신 하나 가지고 시작
         int sum = 0;    //연속된 정수의 합
 
-        for (int i = 1; i <= N/2; i++) {  //시작
+        for (int i = 1; i <= N / 2; i++) {  //시작
             sum = i;
             for (int j = i + 1; j <= N; j++) {     //끝
                 sum += j;
-                if (sum == N) count++;  //연속된 정수들의 합이 N이면 count+1
+                if (sum == N) {
+                    count++;
+                    break;
+                }//연속된 정수들의 합이 N이면 count+1
                 if (sum > N) break;     //합이 N보다 작으면 안쪽 for문 종료
             }
         }
