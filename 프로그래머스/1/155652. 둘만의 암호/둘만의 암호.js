64fb36bda80d10b10942adda0f083bfc42e8dd45
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2024-01-10 00:43:06 +0900

    [level 1] Title: 둘만의 암호, Time: 0.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,20 @@
+function solution(s, skip, index) {
+  var answer = "";
+
+  let arr = Array.from({ length: 26 }, (v, i) => i + 1);
+  for (let i = 0; i < skip.length; i++) {
+    let temp = skip[i].charCodeAt() - 96;
+    arr.splice(arr.indexOf(temp), 1);
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let n = arr.indexOf(s[i].charCodeAt() - 96);
+      if(n+index >= arr.length){
+          n=(n+index)%arr.length;
+      }else{
+          n=n+index;
+      }
+    answer += String.fromCharCode(arr[n] + 96);
+  }
+  return answer;
+}
