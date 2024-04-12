cc5881db131743c6f6f13cc21e9e9c58cff5a8d6
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2024-04-13 01:32:54 +0900

    [level 3] Title: 산 모양 타일링, Time: 35.93 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.js
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.js
@@ -1,22 +1,23 @@
 function solution(n, tops) {
-    var answer = 0;
-    let a = [];
-    let b = [];
-    
-    a[1]=1;
-    b[1]=tops[0]==1 ? 3 : 2
-    
-    for (let i=2;i<=n;i++){
-        a[i]=(a[i-1]+b[i-1])%10007;
+  var answer = 0;
+  const MOD = 10007;
+  let a = [];
+  let b = [];
 
-        if(tops[i-1]==1){
-            b[i]=(2*a[i-1]+3*b[i-1])%10007;
-        }
-        if(tops[i-1]==0){
-            b[i]=(a[i-1]+2*b[i-1])%10007;
-        }
+  a[1] = 1;
+  b[1] = tops[0] == 1 ? 3 : 2;
+
+  for (let i = 2; i <= n; i++) {
+    a[i] = (a[i - 1] + b[i - 1]) % MOD;
+
+    if (tops[i - 1] == 1) {
+      b[i] = (2 * a[i - 1] + 3 * b[i - 1]) % MOD;
+    }   
+    if (tops[i - 1] == 0) {
+      b[i] = (a[i - 1] + 2 * b[i - 1]) % MOD;
     }
-    answer = (a[n]+b[n])%10007;
-    
-    return answer;
+  }
+  answer = (a[n] + b[n]) % MOD;
+
+  return answer;
 }
