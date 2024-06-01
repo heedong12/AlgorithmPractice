aeba17d6bc13d77a62e78f95a7b5192d877c0d2a
HeeYeon_Lee <114553010+heedong12@users.noreply.github.com>
2024-06-02 03:04:30 +0900

    [level 2] Title: 우박수열 정적분, Time: 3.34 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.js
@@ -0,0 +1,23 @@
+function solution(k, ranges) {
+    var answer = [];
+    //우박수열 구하기
+    let area=[];
+    let count =0;
+    while(k!=1){
+        let temp=k;
+        k=k%2==0?k/2:k*3+1;
+        area.push(parseFloat((temp+k)/2));
+        count++;
+    }
+    ranges.map((range)=>{
+        range[1]=count+range[1];
+        if(range[0]>range[1]) return answer.push(-1);
+        let sum=0;
+        for(let i=range[0];i<range[1];i++){
+            sum+=area[i];
+        }
+        return answer.push(sum);
+    })
+    
+    return answer;
+}
