66100d4921c72d024f47175fccd4cba3efeb2778
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2024-06-05 23:45:19 +0900

    [level 3] Title: 여행경로, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -6,18 +6,16 @@ function solution(tickets) {
     
     answer.push("ICN");
     const DFS = (str) => {    
-        if(answer.length === len+1) return;
-        
-        for(let i=0;i<len;i++){
-            if(!visit[i] && str == sortedTickets[i][0]){
-                visit[i]=true;
-                answer.push(sortedTickets[i][1]);
-                DFS(sortedTickets[i][1]);
-                if(answer.length === len+1) return;
-                answer.pop();
-                visit[i]=false;
-            }
-        }
+      for(let i=0;i<len;i++){
+          if(!visit[i] && str == sortedTickets[i][0]){
+              answer.push(sortedTickets[i][1]);
+              visit[i]=true;
+              DFS(sortedTickets[i][1]);
+              if(answer.length == len+1) return;
+              visit[i]=false;
+              answer.pop();
+          }
+      }
     }
     
     DFS("ICN");
