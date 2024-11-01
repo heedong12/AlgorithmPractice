a2134d1323a08bf67bbee4a25127853e75415b0a
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2024-11-02 00:49:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 504 ms, Memory: 84912 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -5,30 +5,26 @@ const input = require("fs")
   .split("\n");
 
 const [N, M] = input[0].split(" ").map(Number);
-let arr = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
-for (let i = 1; i <= N; i++) {
-  let temp = input[i].split(" ").map(Number);
-  for (let j = 1; j <= N; j++) {
-    arr[i][j] = temp[j - 1];
-  }
-}
 
 let sumArr = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
 for (let i = 1; i <= N; i++) {
+  let row = input[i].split(" ").map(Number);
   for (let j = 1; j <= N; j++) {
     sumArr[i][j] =
-      arr[i][j] + sumArr[i - 1][j] + sumArr[i][j - 1] - sumArr[i - 1][j - 1];
+      row[j - 1] + sumArr[i - 1][j] + sumArr[i][j - 1] - sumArr[i - 1][j - 1];
   }
 }
 
+let results = [];
+
 for (let i = N + 1; i < input.length; i++) {
   let [x1, y1, x2, y2] = input[i].split(" ");
-
-  let result =
+  results.push(
     sumArr[x2][y2] -
-    sumArr[x1 - 1][y2] -
-    sumArr[x2][y1 - 1] +
-    sumArr[Math.min(x1, x2) - 1][Math.min(y1, y2) - 1];
-
-  console.log(result);
+      sumArr[x1 - 1][y2] -
+      sumArr[x2][y1 - 1] +
+      sumArr[x1 - 1][y1 - 1]
+  );
 }
+
+console.log(results.join("\n"));
