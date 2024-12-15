4a858d2beb8dff6df343f680f010fb812a1e3f0e
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2024-12-15 23:10:38 +0900

    [level 3] Title: 표 편집, Time: 413.05 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.js
+++ 프로그래머스/3/81303. 표 편집/표 편집.js
@@ -1,37 +1,68 @@
-function solution(n, k, cmd) {
-  const trash = [];
-
-  const up = [...new Array(n + 2)].map((_, i) => i - 1);
-  const down = [...new Array(n + 2)].map((_, i) => i + 1);
-  k++
-  for (const c of cmd) {
-    if (c[0] === "U" || c[0] === "D") {
-      let [upOrDown, num] = c.split(" ");
-      if (upOrDown === "U") {
-        for (let i = 0; i < num; i++) {
-          k = up[k];
-        }
-      } else {
-        for (let i = 0; i < num; i++) {
-          k = down[k];
-        }
-      }
-    } else if (c === "C") {
-      trash.push(k);
-      up[down[k]] = up[k];
-      down[up[k]] = down[k];
-      k = n < down[k] ? up[k] : down[k];
-    } else {
-      let top = trash.pop();
-      down[up[top]] = top;
-      up[down[top]] = top;
+class Node {
+    constructor(value, prev = null, next = null) {
+        this.value = value;
+        this.prev = prev;
+        this.next = next;
     }
-  }
+}
 
-  const answer = new Array(n).fill("O");
-  for (let i of trash) {
-    answer[i - 1] = "X";
-  }
+function solution(n, k, cmd) {
+    const table = new Array(n).fill(true);
+    const nodes = [...new Array(n)].map((_, i) => new Node(i));
+    const trash = [];
+    let index = nodes[k];
 
-  return answer.join("");
+    // 연결리스트 구조 초기화
+    for (let i = 0; i < n; i++) {
+        if (i > 0) nodes[i].prev = nodes[i - 1];
+        if (i < n - 1) nodes[i].next = nodes[i + 1];
+    }
+
+    for (let command of cmd) {
+        let [c, num] = command.split(' ');
+        num = parseInt(num);
+
+        switch (c) {
+            case 'U':
+                for (let i = 0; i < num; i++) {
+                    if (index.prev) {
+                        index = index.prev;
+                    }
+                }
+                break;
+            case 'D':
+                for (let i = 0; i < num; i++) {
+                    if (index.next) {
+                        index = index.next;
+                    }
+                }
+                break;
+            case 'C':
+                table[index.value] = false;
+                trash.push(index);
+
+                // 노드 삭제 처리
+                if (index.next === null) {  // 마지막 노드 삭제
+                    index = index.prev;
+                    index.next = null;
+                } else if (index.prev === null) {  // 첫 번째 노드 삭제
+                    index = index.next;
+                    index.prev = null;
+                } else {  // 중간 노드 삭제
+                    index.prev.next = index.next;
+                    index.next.prev = index.prev;
+                    index = index.next;
+                }
+                break;
+            case 'Z':
+                let restoredNode = trash.pop();
+                table[restoredNode.value] = true;
+
+                if (restoredNode.prev) restoredNode.prev.next = restoredNode;
+                if (restoredNode.next) restoredNode.next.prev = restoredNode;
+                break;
+        }
+    }
+
+    return table.map(item => item ? 'O' : 'X').join('');
 }
