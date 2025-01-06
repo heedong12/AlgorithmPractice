4594d7751c86560211f7e999210ed356fe2595d5
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-01-06 14:34:19 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.js
@@ -5,6 +5,12 @@ function solution(video_len, pos, op_start, op_end, commands) {
         
         return Number(minute)*60 + Number(second);
     }
+    
+    const checkIsOpening = (time) => {
+        if(pos<=op_end && pos>=op_start) {
+            pos = op_end
+        }
+    }
 
     video_len = changeTimeToSecond(video_len)
     pos = changeTimeToSecond(pos)
@@ -12,10 +18,7 @@ function solution(video_len, pos, op_start, op_end, commands) {
     op_end = changeTimeToSecond(op_end)
     
     commands.forEach(item => {
-         // 오프닝 범위
-        if(pos<=op_end && pos>=op_start) {
-            pos = op_end
-        }
+        checkIsOpening(pos)
         
         if(item === 'prev'){
             pos = pos-10 < 0 ? 0 : pos-10
@@ -25,16 +28,10 @@ function solution(video_len, pos, op_start, op_end, commands) {
         
     })
     
-     // 오프닝 범위
-    if(pos<=op_end && pos>=op_start) {
-        pos = op_end
-    }
+    checkIsOpening(pos)
     
-    let min = Math.floor(pos/60)
-    let sec = pos % 60
-
-    min = min.toString().padStart(2,'0')
-    sec = sec.toString().padStart(2,'0')
+    let min = Math.floor(pos/60).toString().padStart(2,'0')
+    let sec = (pos % 60).toString().padStart(2,'0')
     
-    return min+":"+sec;
+    return min + ":" + sec;
 }
