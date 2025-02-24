f26a51796a69b56899f8746fecc133e2928e519d
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-02-24 09:24:32 +0900

    [Bronze I] Title: 캠핑, Time: 92 ms, Memory: 9948 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.js
+++ 백준/Bronze/4796. 캠핑/캠핑.js
@@ -9,7 +9,7 @@ const answer = [];
 for (let i = 0; i < inputs.length - 1; i++) {
   let [L, P, V] = inputs[i].split(" ").map(Number);
   let rest = Math.floor(V / P);
-  let addRest = V - rest * P >= L ? L : V - rest * P;
+  let addRest = Math.min(V - rest * P, L)
 
   answer.push(`Case ${i + 1}: ${rest * L + addRest}`);
 }
