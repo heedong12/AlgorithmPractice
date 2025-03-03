ec0e7ba60f323cbd930a9cbd2581aa4b242fd64c
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-03-03 17:37:37 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 104 ms, Memory: 9856 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
@@ -5,18 +5,19 @@ const [T, ...inputs] = require("fs")
   .split("\n")
   .map(Number);
 
+const dp = Array.from({ length: 15 }, () => new Array(15));
+
+for (let j = 0; j < dp.length; j++) {
+  for (let k = 0; k < dp[0].length; k++) {
+    if (k <= 1) dp[j][k] = 1;
+    else if (j === 0) dp[j][k] = k;
+    else dp[j][k] = dp[j - 1][k] + dp[j][k - 1];
+  }
+}
+
 for (let i = 0; i < inputs.length; i = i + 2) {
   let K = inputs[i];
   let N = inputs[i + 1];
 
-  const dp = Array.from({ length: K + 1 }, () => new Array(N + 1));
-
-  for (let j = 0; j < dp.length; j++) {
-    for (let k = 0; k < dp[0].length; k++) {
-      if (k <= 1) dp[j][k] = 1;
-      else if (j === 0) dp[j][k] = k;
-      else dp[j][k] = dp[j - 1][k] + dp[j][k - 1];
-    }
-  }
   console.log(dp[K][N]);
 }
