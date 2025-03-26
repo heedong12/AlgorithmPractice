492042daa609c9fa4d323fba5674c75bd2fcb20c
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-03-26 10:37:13 +0900

    [Silver III] Title: 두 수의 합, Time: 208 ms, Memory: 27436 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -0,0 +1,24 @@
+let [N, nums, M] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+N = Number(N);
+M = Number(M);
+
+const set = new Set(nums.split(" ").map(Number));
+
+let count = 0;
+
+for (let num of set) {
+  let target = M - num;
+
+  if (set.has(target) && num !== target) {
+    count++;
+    set.delete(num);
+    set.delete(target);
+  }
+}
+
+console.log(count);
