400bff8b768df7e04193038e3442480826ccf75e
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-04-06 17:13:43 +0900

    [Gold IV] Title: DSLR, Time: 9004 ms, Memory: 46728 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.js
+++ 백준/Gold/9019. DSLR/DSLR.js
@@ -4,26 +4,27 @@ let [T, ...arr] = require("fs")
   .trim()
   .split("\n");
 
-const D = (num) => (2 * num) % 10000;
-const S = (num) => (num === 0 ? 9999 : num - 1);
-const L = (num) => {
-  return Number((num % 1000) * 10 + Math.floor(num / 1000));
-};
-const R = (num) => {
-  return Number((num % 10) * 1000 + Math.floor(num / 10));
-};
-const METHODS = new Map([
-  ["D", D],
-  ["S", S],
-  ["L", L],
-  ["R", R],
-]);
+// const D = (num) => (2 * num) % 10000;
+// const S = (num) => (num === 0 ? 9999 : num - 1);
+// const L = (num) => {
+//   return Number((num % 1000) * 10 + Math.floor(num / 1000));
+// };
+// const R = (num) => {
+//   return Number((num % 10) * 1000 + Math.floor(num / 10));
+// };
+// const METHODS = new Map([
+//   ["D", D],
+//   ["S", S],
+//   ["L", L],
+//   ["R", R],
+// ]);
 
 let answers = [];
 const bfs = (A, B) => {
-  // 숫자와 명령어 저장
   let visited = new Array(10000).fill(false);
+  // 숫자와 명령어 저장
   const queue = [[A, ""]];
+  // 방문한 숫자에는 방문 표시
   visited[A] = true;
   let answer = "";
 
@@ -35,12 +36,28 @@ const bfs = (A, B) => {
       break;
     }
 
-    for (let [string, func] of METHODS) {
-      let result = func(num);
-      if (!visited[result]) {
-        visited[result] = true;
-        queue.push([result, command + string]);
-      }
+    let D = (2 * num) % 10000;
+    if (!visited[D]) {
+      queue.push([D, command + "D"]);
+      visited[D] = true;
+    }
+
+    let S = num === 0 ? 9999 : num - 1;
+    if (!visited[S]) {
+      queue.push([S, command + "S"]);
+      visited[S] = true;
+    }
+
+    let L = (num % 1000) * 10 + Math.floor(num / 1000);
+    if (!visited[L]) {
+      queue.push([L, command + "L"]);
+      visited[L] = true;
+    }
+
+    let R = (num % 10) * 1000 + Math.floor(num / 10);
+    if (!visited[R]) {
+      queue.push([R, command + "R"]);
+      visited[R] = true;
     }
   }
   return answer;
