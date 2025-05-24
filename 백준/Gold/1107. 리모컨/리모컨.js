be48a609e37b69007397e6a2326ad083ae9fe8a5
Lee_HeeYeon <114553010+heedong12@users.noreply.github.com>
2025-05-24 18:52:41 +0900

    [Gold IV] Title: 리모컨, Time: 456 ms, Memory: 28008 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.js
+++ 백준/Gold/1107. 리모컨/리모컨.js
@@ -4,33 +4,17 @@ let [N, ...inputs] = require("fs")
   .trim()
   .split("\n");
 
-let buttons = [];
-
-if (inputs.length === 2) {
-  buttons = inputs[1].split(" ");
-}
-
-let cur = 100;
-
-let canBtn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].filter(
-  (num) => !buttons.includes(num)
-);
-let min = Math.abs(+N - cur);
-const findNum = (num) => {
-  if (num.length > N.length + 1) return;
-  if (num.length > 0) {
-    const pressCount = num.length + Math.abs(+num - +N);
-    min = Math.min(min, pressCount);
-  }
-
-  for (let i = 0; i < canBtn.length; i++) {
-    findNum(num + canBtn[i]);
-  }
-};
-
-if (+N === 100) {
+if (N === "100") {
   console.log(0);
 } else {
-  findNum("");
+  let broken = inputs.length === 2 ? inputs[1].split(" ") : [];
+  let min = Math.abs(100 - +N);
+
+  for (let i = 0; i <= 1000000; i++) {
+    let str = i.toString();
+    let canMake = str.split("").every((item) => !broken.includes(item));
+
+    if (canMake) min = Math.min(min, Math.abs(i - N) + str.length);
+  }
   console.log(min);
 }
